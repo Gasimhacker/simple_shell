387b0627d051d339e20f221db35e113d68f94fb2
Obada A.Albadee <obadabadee3@hotmail.com>
2023-09-26 22:16:20 +0300

    Update search and esecute + interactive mood

--- run_interactive.c
+++ run_interactive.c
@@ -11,31 +11,25 @@ void run_interactive(alias_t **head, char *shell_name)
 {
 	char **args, *full_path;
 
+	char *buff = NULL;
+    int is_seperator;
+    size_t n;
+
 	while (1)
 	{
 		write(STDOUT_FILENO, "$ ", 2);
+        
+	    if (getline(&buff, &n, stdin) == -1)
+		    exit(1);
+        
+        is_seperator = check_separator(buff);
 
-		args = create_args();
-
-		if (args == NULL)
-			continue;
-
-		if (search_builtins(head, shell_name, args[0], args))
-		{
-			clean(args);
-			continue;
-		}
-
-		full_path = find_file(args[0]);
-
-		if (full_path == NULL)
-		{
-			cmd_not_found_msg(shell_name, args[0]);
-			clean(args);
-			continue;
-		}
-
-		execute(args, full_path);
+        if (is_seperator)
+            continue;
+        else
+            search_execute(buff);
+            
+        free(buff);
 	}
 
 }
