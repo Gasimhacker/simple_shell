15c0589d6752c3855ae48ff046928d62359182a1
Obada A.Albadee <obadabadee3@hotmail.com>
2023-09-27 01:27:24 +0300

    Free Commands

--- handle_separator.c
+++ handle_separator.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * check_separator - Searh a buffer for some seprator 
+ * check_separator - Searh a buffer for some seprator
  * @buff: Buffer to be searched for a seprator
  * @head: The head of the linked list that needs to be freed
  * @shell_name: The name of the shell is used for printing error messages
@@ -10,23 +10,27 @@
 int check_separator(char *buff, alias_t **head, char *shell_name)
 {
     char **commands;
-    if (_strstr(buff,";"))
+    if (_strstr(buff, ";"))
     {
-        commands = split_string(buff,";");
-        handle_colon(commands,head,shell_name);
+        commands = split_string(buff, ";");
+        handle_colon(commands, head, shell_name);
     }
-    else if (_strstr(buff,"&&"))
+    else if (_strstr(buff, "&&"))
     {
-        commands = split_string(buff,"&&");
-        handle_anding(commands,head,shell_name);
+        commands = split_string(buff, "&&");
+        handle_anding(commands, head, shell_name);
     }
-    else if (_strstr(buff,"||"))
+    else if (_strstr(buff, "||"))
     {
-        commands = split_string(buff,"||");
-        handle_oring(commands,head,shell_name);
+        commands = split_string(buff, "||");
+        handle_oring(commands, head, shell_name);
     }
     else
-        return(0);
+    {
+        Free(commands);
+        return (0);
+    }
+    Free(commands);
     return (1);
 }
 
@@ -42,12 +46,16 @@ void handle_anding(char **commands, alias_t **head, char *shell_name)
     int *last_exit_code;
     int i = 0;
 
-    search_execute(commands[i++],head,shell_name);
+    search_execute(commands[i], head, shell_name);
+    Free(commands[i++]);
     while (commands[i])
     {
         last_exit_code = get_exit_status();
         if (*last_exit_code == EXIT_SUCCESS)
-            search_execute(commands[i++],head,shell_name);
+        {
+            search_execute(commands[i], head, shell_name);
+            Free(commands[i++]);
+        }
         else
             break;
     }
@@ -62,17 +70,21 @@ void handle_anding(char **commands, alias_t **head, char *shell_name)
  */
 void handle_oring(char **commands, alias_t **head, char *shell_name)
 {
-    int *last_exit_code ;
+    int *last_exit_code;
     int i = 0;
 
-    search_execute(commands[i++],head,shell_name);
+    search_execute(commands[i], head, shell_name);
+    Free(commands[i++]);
     while (commands[i])
     {
         last_exit_code = get_exit_status();
         if (*last_exit_code == EXIT_SUCCESS)
             break;
         else
-            search_execute(commands[i++],head,shell_name);
+        {
+            search_execute(commands[i], head, shell_name);
+            Free(commands[i++]);
+        }
     }
 }
 
@@ -83,12 +95,15 @@ void handle_oring(char **commands, alias_t **head, char *shell_name)
  * @shell_name: The name of the shell is used for printing error messages
  * Return: void
  */
-void handle_colon(char **commands,alias_t **head, char *shell_name)
+void handle_colon(char **commands, alias_t **head, char *shell_name)
 {
     int i = 0;
 
     while (commands[i])
-        search_execute(commands[i++],head,shell_name);
+    {
+        search_execute(commands[i], head, shell_name);
+        Free(commands[i++]);
+    }
 }
 
 /**
@@ -101,27 +116,26 @@ void handle_colon(char **commands,alias_t **head, char *shell_name)
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int index;
+    int index;
 
-	if (*needle == 0)
-	{
-		return (haystack);
-	}
-	while (*haystack)
-	{
-		index = 0;
+    if (*needle == 0)
+    {
+        return (haystack);
+    }
+    while (*haystack)
+    {
+        index = 0;
 
-		while (*(haystack + index) == needle[index] && needle[index])
-		{
-			if (needle[index + 1] == '\0')
-			{
-				return (haystack);
-			}
-			index++;
-		}
-		haystack++;
+        while (*(haystack + index) == needle[index] && needle[index])
+        {
+            if (needle[index + 1] == '\0')
+            {
+                return (haystack);
+            }
+            index++;
+        }
+        haystack++;
+    }
 
-	}
-
-	return (0);
+    return (0);
 }
